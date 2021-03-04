9ced1a220ce0d4447ee714bfa467681b4e518416
swu ye <ye79_2020@qq.com>
2021-03-04 22:59:14 +0800

    3-2 22：12

--- pytorch_api_linear.py
+++ pytorch_api_linear.py
@@ -1,53 +1,35 @@
 import torch
+#model实现自己的类，继承module模块
+class Model(torch.nn.Module):
+    #继承他的原因是初始化的调用函数要使用他
+    def __init__(self):
+        #调用父辈的同名方法都是这样的调用 super（本类名，self）.方法
+        # python中的super( test, self).init()
+        # 首先找到test的父类（比如是类A），然后把类test的对象self转换为类A的对象，然后“被转换”的类A对象调用自己的__init__函数.
+        super(Model,self).__init__()
+        #实例中添加linear函数，使用torch中linear函数，返回得到一个linear对象
+        self.linear=torch.nn.Linear(1,1)
+    def forward(self,x):
+        #实现正向传播图 调用实例对象的linear对象，即上面初始化的对象
+        return self.linear(x)
 
-# prepare dataset
-# x,y是矩阵，3行1列 也就是说总共有3个数据，每个数据只有1个特征
-#将数据打到就可以知道数据得到个数和特征个数，矩阵应该写成 个数*特征
+
+model=Model()
+
+#此处loss和optim都是可调用对象，即无参构造，传入参数调用
+loss=torch.nn.MSELoss(reduction="sum")
+optim=torch.optim.SGD(model.parameters(),lr=0.01)
+
+
 x_data = torch.Tensor([[1.0], [2.0], [3.0]])
 y_data = torch.Tensor([[2.0], [4.0], [6.0]])
 
-# design model using class
-"""
-our model class should be inherit from nn.Module, which is base class for all neural network modules.
-member methods __init__() and forward() have to be implemented
-class nn.linear contain two member Tensors: weight and bias
-class nn.Linear has implemented the magic method __call__(),which enable the instance of the class can
-be called just like a function.Normally the forward() will be called 
-"""
-
-
-class LinearModel(torch.nn.Module):
-    def __init__(self):
-        super(LinearModel, self).__init__()
-        # (1,1)是指输入x和输出y的特征维度，这里数据集中的x和y的特征都是1维的
-        # 该线性层需要学习的参数是w和b  获取w/b的方式分别是~linear.weight/linear.bias
-        self.linear = torch.nn.Linear(1, 1)
-
-    def forward(self, x):
-        y_pred = self.linear(x)
-        return y_pred
-
-
-model = LinearModel()
-
-# construct loss and optimizer
-# criterion = torch.nn.MSELoss(size_average = False)
-criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)  # model.parameters()自动完成参数的初始化操作
-
-# training cycle forward, backward, update
-for epoch in range(100):
-    y_pred = model(x_data)  # forward:predict
-    loss = criterion(y_pred, y_data)  # forward: loss
-    print(epoch, loss.item())
-
-    optimizer.zero_grad()  # the grad computer by .backward() will be accumulated. so before backward, remember set the grad to zero
-    loss.backward()  # backward: autograd，自动计算梯度
-    optimizer.step()  # update 参数，即更新w和b的值
-
-print('w = ', model.linear.weight.item())
-print('b = ', model.linear.bias.item())
-
-x_test = torch.Tensor([[4.0]])
-y_test = model(x_test)
-print('y_pred = ', y_test.data)
+for epoch in range(1,1000):
+    y=model(x_data)
+    cost=loss(y,y_data)
+    print(epoch,":",cost.data.item(),cost.data.item())
+    optim.zero_grad()
+    cost.backward()
+    optim.step()
+print(model.linear.weight.item())
+print(model.linear.bias.item())
